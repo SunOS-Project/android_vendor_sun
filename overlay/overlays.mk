fe2d12206b45a708685ed3d9afd6bd512e746934
Omkar Chandorkar <gotenksIN@aosip.dev>
2021-11-30 08:35:49 +0100

    overlay: Import Linotte font
    * From https://github.com/yaap/vendor_themes/commit/b5502cc6b10f9cb3a61cd7f9778f8a30e62147a4
    
    Change-Id: I57e29d75eb3ed27cb649eacffd30566314322b77
    Co-authored-by: Jyotiraditya Panda <jyotiraditya@aospa.co>
    Co-authored-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Co-authored-by: Pranav Vashi <neobuddy89@gmail.com>
    Co-authored-by: idoybh <idoybh2@gmail.com>

--- overlay/overlays.mk
+++ overlay/overlays.mk
@@ -14,6 +14,7 @@ PRODUCT_PACKAGES += \
     FontGoogleSansOverlay \
     FontHarmonySansOverlay \
     FontInterOverlay \
+    FontLinotteSourceOverlay \
     FontManropeOverlay \
     FontOneplusSansOverlay \
     FontOppoSansOverlay \
