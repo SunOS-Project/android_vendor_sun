bf48aae2bbfc4f2a3e53ec4b395439e149131c97
Karan Parashar <karan@pixelos.net>
2022-12-01 04:32:54 +0000

    common: Add google dialer call recording feature
    Change-Id: Ifc8ab79aa5bfee81774af96943d668074fc36ee5
    Signed-off-by: Frost <frost.github@proton.me>

--- config/common.mk
+++ config/common.mk
@@ -17,6 +17,14 @@ endif
 PRODUCT_PACKAGES += \
     bootanimation_pixel
 
+# Call Recording
+TARGET_CALL_RECORDING_SUPPORTED ?= true
+
+ifneq ($(TARGET_CALL_RECORDING_SUPPORTED),false)
+PRODUCT_COPY_FILES += \
+    vendor/lineage/config/permissions/com.google.android.apps.dialer.call_recording_audio.features.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/com.google.android.apps.dialer.call_recording_audio.features.xml
+endif
+
 # Charger
 WITH_LINEAGE_CHARGER := false
 PRODUCT_PACKAGES += \
