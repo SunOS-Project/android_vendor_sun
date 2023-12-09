d2986af4f4170c532900cf859077efaa01b47f44
Edwin Moquete <edwinmmoquete@gmail.com>
2023-12-09 17:10:29 +0000

    config: Disable display frame rate override
    This got enabled by default on U, and it causes apps like
    Chrome and Youtube to set the refresh rate to 30FPS when
    playing some videos.
    
    Change-Id: I649bf03d550c2b9726c7957d15ed09e455d874ec
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -40,7 +40,8 @@ PRODUCT_SYSTEM_EXT_PROPERTIES += \
 
 # Display
 PRODUCT_SYSTEM_EXT_PROPERTIES += \
-    debug.sf.frame_rate_multiple_threshold=60
+    debug.sf.frame_rate_multiple_threshold=60 \
+    ro.surface_flinger.enable_frame_rate_override=false
 
 # Enable one-handed mode
 PRODUCT_PRODUCT_PROPERTIES += \
