7fd5206a7227b28c6961c7ec9bd5cf7c27d8d023
AdarshGrewal <adarshgrewal@gmail.com>
2025-08-04 23:31:50 +0530

    overlay: Import Default wallpapers
    Change-Id: Id7f16d888a57edc6974ed11a2597b58ef9236608

--- config/common.mk
+++ config/common.mk
@@ -217,6 +217,7 @@ PRODUCT_PACKAGE_OVERLAYS += \
     vendor/lineage/overlay/no-rro
 
 PRODUCT_PACKAGES += \
+    FrameworkOverlayCustom \
     NetworkStackOverlay \
     PermissionControllerOverlay \
     SettingsOverlayCustom
