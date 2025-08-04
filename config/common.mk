602a165f8a0dcb0ee7e5ce343b843f8dc0298722
AdarshGrewal <adarshgrewal@gmail.com>
2025-08-05 00:47:26 +0530

    overlay: Import lottie animations from caiman SettingsGoogle
    Change-Id: I622320692cc2cf820b2f17929fa204d65fdc1f56

--- config/common.mk
+++ config/common.mk
@@ -240,7 +240,8 @@ PRODUCT_PACKAGE_OVERLAYS += \
 
 PRODUCT_PACKAGES += \
     NetworkStackOverlay \
-    PermissionControllerOverlay
+    PermissionControllerOverlay \
+    SettingsOverlayCustom
 
 # Translations
 CUSTOM_LOCALES += \
