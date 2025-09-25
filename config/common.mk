pissel-os/android_vendor_custom

config: bootanimation: Import from Pixel
 bootanimation: Update from caiman
 Author: basamaryan <basam.aryan@gmail.com>
 Date: Tue Aug 13 19:23:39 2024 +0200
 Change-Id: Ibfff51cd18d463ded830a18dff1ef190880769d5

 bootanimation: chmod 0644 bootanimation_720.zip
 Change-Id: I8f7b4833459dbd041b3502209de177573cf9c671
 Author: Hridaya Prajapati <info.hridayaprajapati@gmail.com>
 Date: Mon Apr 28 10:34:00 2025 +0545

Change-Id: I6951a103b5ad4c0ba86b1989bd0ae4c121e580e6

config/common.mk
@@ -1,1 +1,4 @@
 # Common PixelOS stuff.
+
+# Bootanimation
+include vendor/custom/config/bootanimation.mk

SunOS-Project/android_vendor_sun

config/common.mk
@@ -4,6 +4,9 @@ $(call inherit-product-if-exists, vendor/extra/product.mk)
 # Allow vendor prebuilt repos to exclude themselves from bp scanning
 -include $(sort $(wildcard vendor/*/*/exclude-bp.mk))
 
+# Bootanimation
+include vendor/lineage/config/bootanimation.mk
+
 # GMS
 include vendor/lineage/config/pixel.mk
 