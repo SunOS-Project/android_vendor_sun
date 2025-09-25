pissel-os/android_vendor_custom

config: Setup pixel repos
Change-Id: I8dabeb316aba6dcfe3da87bc40944e006862841e

config/common.mk
@@ -2,3 +2,6 @@
 
 # Bootanimation
 include vendor/custom/config/bootanimation.mk
+
+# GMS
+include vendor/custom/config/pixel.mk

SunOS-Project/android_vendor_sun

config/common.mk
@@ -4,6 +4,9 @@ $(call inherit-product-if-exists, vendor/extra/product.mk)
 # Allow vendor prebuilt repos to exclude themselves from bp scanning
 -include $(sort $(wildcard vendor/*/*/exclude-bp.mk))
 
+# GMS
+include vendor/lineage/config/pixel.mk
+
 PRODUCT_BRAND ?= SunOS
 
 ifeq ($(PRODUCT_GMS_CLIENTID_BASE),)