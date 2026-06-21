PixelOS-AOSP/android_vendor_custom

overlay: tablet: Enable config_canInternalDisplayHostDesktops
Change-Id: I6adc331da58bcfd32a46a40be91f096cf3c101a5

config/common.mk
@@ -62,6 +62,11 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGE_OVERLAYS += \
     vendor/custom/overlay/common
 
+ifeq ($(TARGET_IS_TABLET),true)
+PRODUCT_PACKAGE_OVERLAYS += \
+    vendor/custom/overlay/tablet
+endif
+
 PRODUCT_PACKAGES += \
     FrameworkOverlayCustom \
     SettingsOverlayCustom

SunOS-Project/android_vendor_sun

config/common.mk
@@ -298,6 +298,11 @@ PRODUCT_PACKAGE_OVERLAYS += \
     vendor/lineage/overlay/common \
     vendor/lineage/overlay/no-rro
 
+ifeq ($(TARGET_IS_TABLET),true)
+PRODUCT_PACKAGE_OVERLAYS += \
+    vendor/lineage/overlay/tablet
+endif
+
 PRODUCT_PACKAGES += \
     FrameworkOverlayCustom \
     NetworkStackOverlay \