pissel-os/android_vendor_custom

config: bootanimation: Import from Pixel
 bootanimation: Update from caiman
 Author: basamaryan <basam.aryan@gmail.com>
 Date: Tue Aug 13 19:23:39 2024 +0200
 Change-Id: Ibfff51cd18d463ded830a18dff1ef190880769d5

 bootanimation: chmod 0644 bootanimation_720.zip
 Change-Id: I8f7b4833459dbd041b3502209de177573cf9c671
 Author: Hridaya Prajapati <info.hridayaprajapati@gmail.com>
 Date: Mon Apr 28 10:34:00 2025 +0545

Change-Id: I6951a103b5ad4c0ba86b1989bd0ae4c121e580e6

config/bootanimation.mk
@@ -1,0 +1,15 @@
+# Bootanimation
+ifeq ($(TARGET_BOOT_ANIMATION_RES),1080)
+     PRODUCT_COPY_FILES += vendor/custom/bootanimation/bootanimation_1080.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
+else ifeq ($(TARGET_BOOT_ANIMATION_RES),1440)
+     PRODUCT_COPY_FILES += vendor/custom/bootanimation/bootanimation_1440.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
+else ifeq ($(TARGET_BOOT_ANIMATION_RES),720)
+     PRODUCT_COPY_FILES += vendor/custom/bootanimation/bootanimation_720.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
+else
+    ifeq ($(TARGET_BOOT_ANIMATION_RES),)
+        $(warning "TARGET_BOOT_ANIMATION_RES is undefined, assuming 1080p")
+    else
+        $(warning "Current bootanimation res is not supported, forcing 1080p")
+    endif
+    PRODUCT_COPY_FILES += vendor/custom/bootanimation/bootanimation_1080.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
+endif

config/common.mk
@@ -1,1 +1,4 @@
 # Common PixelOS stuff.
+
+# Bootanimation
+include vendor/custom/config/bootanimation.mk

SunOS-Project/android_vendor_sun

config/bootanimation.mk
@@ -1,0 +1,15 @@
+# Bootanimation
+ifeq ($(TARGET_BOOT_ANIMATION_RES),1080)
+     PRODUCT_COPY_FILES += vendor/lineage/bootanimation/bootanimation_1080.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
+else ifeq ($(TARGET_BOOT_ANIMATION_RES),1440)
+     PRODUCT_COPY_FILES += vendor/lineage/bootanimation/bootanimation_1440.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
+else ifeq ($(TARGET_BOOT_ANIMATION_RES),720)
+     PRODUCT_COPY_FILES += vendor/lineage/bootanimation/bootanimation_720.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
+else
+    ifeq ($(TARGET_BOOT_ANIMATION_RES),)
+        $(warning "TARGET_BOOT_ANIMATION_RES is undefined, assuming 1080p")
+    else
+        $(warning "Current bootanimation res is not supported, forcing 1080p")
+    endif
+    PRODUCT_COPY_FILES += vendor/lineage/bootanimation/bootanimation_1080.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
+endif

config/common.mk
@@ -7,6 +7,9 @@ PRODUCT_SOURCE_ROOT_DIRS += -kernel/platform
 # Allow vendor prebuilt repos to exclude themselves from bp scanning
 -include $(sort $(wildcard vendor/*/*/exclude-bp.mk))
 
+# Bootanimation
+include vendor/lineage/config/bootanimation.mk
+
 # GMS
 include vendor/lineage/config/pixel.mk
 